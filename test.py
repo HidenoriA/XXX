4a5fad162d8c42d45922883d9bfded0046a27ee9
HidenoriA <hidenori.ide@gmail.com>
2017-02-20 14:50:59 +0900

    バグ修正版

--- test.py
+++ test.py
@@ -1,15 +1,12 @@
-port pandas as pd/Users/hidenori/Downloads/XXX/test.py
-
-df = pd.read_csv("training_data.csv")
+import pandas as pd
+df = pd.read_csv('training_data.csv')
 
 df.term=df.term.str.extract('([0-9]+)')
 df.int_rate=df.int_rate.str.extract('([0.0-9]+)')
 df.zip_code=df.zip_code.str.extract('([0.0-9]+)')
 df.revol_util=df.revol_util.str.extract('([0.0-9]+)')
 df.revol_util=df.revol_util.str.extract('([0.0-9]+)')
-#置き換え
-#df['emp_length'] =df['emp_length'].mask(df['amp_length']=='< 1 year’,’0’)
-df['emp_length'] = df['emp_length'].mask(df['amp_length']=='< 1 year','0')
+df['emp_length'] =df['emp_length'].mask(df['emp_length']=='< 1 year','0')
 df['emp_length'] =df['emp_length'].mask(df['emp_length']=='10+ years','10')
 df['emp_length']=df['emp_length'].mask(df['emp_length']=='n/a','0')
 df.emp_length=df.emp_length.str.extract('([0-9]+)')
